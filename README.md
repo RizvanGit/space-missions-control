d8977633dca478ecb65a2e1baf6ae26a295ea87f
RizvanGit <imeandmyep2@gmail.com>
2023-10-23 15:42:49 +0300

    build: Front-end & Back-end initial structure, passing csv data to front

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fullstack Space Mission Control Application
---
+++ server/src/models/planets.model.js
@@ -0,0 +1,45 @@
+const fs = require("fs")
+const path = require("path")
+
+const { parse } = require("csv-parse")
+
+const habitablePlanets = []
+
+function isHabitablePlanet(planet){
+  var disposition = planet["koi_disposition"] === "CONFIRMED"
+  var insol = planet["koi_insol"] > 0.36 && planet["koi_insol"] <1.11
+  var radius = planet["koi_prad"] < 1.6
+
+  return disposition && insol && radius
+}
+
+function loadPlanetsData () {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"))
+      .pipe(parse({
+        comment: "#",
+        columns: true,
+      }))
+      .on("data", (data) => {
+        if(isHabitablePlanet(data)){
+          habitablePlanets.push(data)
+        }
+      })
+      .on("error", (error) => {
+        console.error("An error occurred! Error: ", error)
+        reject(error)
+      })
+      .on("end", () => {
+        console.log(habitablePlanets.map((planet) => {
+          return planet["kepler_name"]
+        }))
+        console.log("COMPLETE! Habitable planets: ", habitablePlanets.length)
+        resolve()
+      })
+  })
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};

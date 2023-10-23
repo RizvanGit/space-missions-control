989b642dfa666a13e12a724b1fdb7f73ccc12dac
RizvanGit <imeandmyep2@gmail.com>
2023-10-23 21:25:11 +0300

    build: morgan for logs, feat: fetching launches.model data for upcoming page

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
-const whitelist = ["http://localhost:3000"]
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8000",
+  "[::1]:8000",
+  "http://127.0.0.1:8000",
+];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if(whitelist.indexOf(origin) !== -1){
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-  }),
-);
+const corsOptionsDelegate = function (req, callback) {
+  const origin = req.header("Origin");
+  const corsOptions = { origin: false };
+
+  if (whitelist.indexOf(origin) !== -1) {
+    corsOptions.origin = true;
+  }
+
+  callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
+app.use(morgan("short"));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+app.use(launchesRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
